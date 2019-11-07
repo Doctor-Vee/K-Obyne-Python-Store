426c9afb5f1e6804d36fb6a0a70ca0db9c3cf2e0
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-11-07 13:46:49 +0100

    Create signup route

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -9,6 +9,70 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__);
 
+
+#Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///kobstore.db")
+
 @app.route("/")
 def index():
-  return render_template("index.html", author="Doctor Vee")
+  return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username").strip()
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        # Ensure all fields were submitted
+        if not username or not password or not confirmation or not email:
+            return render_template ("register.html", msg="all fields must be filled")
+        # ensure passwords match
+        if password != confirmation:
+            return render_template ("register.html", msg="passwords must match")
+        
+        #ensure username and email are unique
+        else:
+            rows = db.execute("SELECT * FROM users WHERE username = :username OR email =:email",
+                              username=username, email=email)
+
+            # Check if username/email already exist
+            if len(rows) > 0:
+                return render_template("register.html", msg="username/email already exists")
+
+            hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+            db.execute("INSERT INTO users (username, password, email) VALUES (:username, :password, :email)",
+                       username=username, password=hash, email=email)
+
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
