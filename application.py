df664bac02b263bb446e75aa639951193cbdedee
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-11-07 08:57:45 +0100

    Setup environment, dependencies and display index

--- application.py
+++ application.py
@@ -1,2 +1,14 @@
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__);
+
+@app.route("/")
+def index():
+  return render_template("index.html", author="Doctor Vee")
